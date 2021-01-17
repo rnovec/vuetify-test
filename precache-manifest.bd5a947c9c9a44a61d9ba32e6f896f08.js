5bf8b6de231404836e9b1198dd1d797b5060b179
Raul Novelo <rnovelo.cruz98@gmail.com>
2021-01-17 13:29:41 -0600

    Updates

---
+++ precache-manifest.bd5a947c9c9a44a61d9ba32e6f896f08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58dbf3db5b663e43ce2d",
+    "url": "/vuetify-test/css/chunk-vendors.77e1c8b6.css"
+  },
+  {
+    "revision": "75b2566768f19cc98d41020015991a0e",
+    "url": "/vuetify-test/index.html"
+  },
+  {
+    "revision": "e6143e3ff7544cbc0504",
+    "url": "/vuetify-test/js/app.bd1548bb.js"
+  },
+  {
+    "revision": "58dbf3db5b663e43ce2d",
+    "url": "/vuetify-test/js/chunk-vendors.ec484ac5.js"
+  },
+  {
+    "revision": "4d4715733d616f6426153f2dfa700d00",
+    "url": "/vuetify-test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vuetify-test/robots.txt"
+  },
+  {
+    "revision": "65dc94066f60f9c6233a1522e553b772",
+    "url": "/vuetify-test/service-worker.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,40 @@
+importScripts("/vuetify-test/precache-manifest.bd5a947c9c9a44a61d9ba32e6f896f08.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.setConfig({
+  debug: false
+})
+
+workbox.precaching.precacheAndRoute([])
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+      })
+    ]
+  })
+)
+
+// workbox.routing.registerRoute(
+//   new RegExp('https://digital-window.herokuapp.com'),
+//   workbox.strategies.networkFirst({
+//     cacheName: 'api'
+//   })
+// )
+
+workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'googleapis',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30
+      })
+    ]
+  })
+)
+
